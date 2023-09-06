9c1905f9a3dbc4c9527d84f516bde61701bb8632
emmanuelCode <e96e6@hotmail.com>
2023-09-05 21:24:49 -0400

    chore: rearrange signUp/login layout and fix gradle error

--- lib/views/signup_or_login_screen.dart
+++ lib/views/signup_or_login_screen.dart
@@ -1,6 +1,7 @@
 // TODO to create login form and email
 import 'package:flutter/material.dart';
 import 'package:flutter_firebase_graphql/models/post.dart';
+import 'package:flutter_firebase_graphql/views/list_post_screen.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../models/auth.dart';
@@ -12,73 +13,100 @@ class SignUpOrLogin extends ConsumerStatefulWidget {
   SignUpOrLoginState createState() => SignUpOrLoginState();
 }
 
-class SignUpOrLoginState extends ConsumerState<SignUpOrLogin>
-    with SingleTickerProviderStateMixin {
-  late TabController _tabController;
+class SignUpOrLoginState extends ConsumerState<SignUpOrLogin> {
   late bool _login;
   late Auth _auth;
 
-  // TODO add TextEditingController for formfield
   final _textEditName = TextEditingController();
   final _textEditEmail = TextEditingController();
   final _textEditPass = TextEditingController();
-
-  _handleTabChange() {
-    if (_tabController.index == 0) {
-      setState(() => _login = true);
-    } else {
-      setState(() => _login = false);
-    }
-  }
 
   @override
   void initState() {
     super.initState();
     _login = true;
-    _tabController = TabController(length: 2, vsync: this);
-    _tabController.addListener(_handleTabChange);
     _auth = ref.read(authProvider.notifier);
   }
 
-  @override
-  void dispose() {
-    _tabController.removeListener(_handleTabChange);
-    _tabController.dispose();
-    super.dispose();
+  InputDecoration _decoration(String value) {
+    return InputDecoration(
+        border: const OutlineInputBorder(), labelText: value);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Firebase/GraphQL')),
+      appBar: AppBar(
+        title: const Text('Firebase/GraphQL'),
+      ),
       body: SafeArea(
-        child: DefaultTabController(
-          length: 2,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              if (!_login) TextFormField(controller: _textEditName),
-              TextFormField(controller: _textEditEmail),
-              TextFormField(controller: _textEditPass),
-              OutlinedButton(
-                onPressed: () async {
-                  _login
-                      ? await _auth.logIn('dash@email.com', 'dashword')
-                      : await _auth.signUp(
-                          'Dash', 'dash@email.com', 'dashword');
-                },
-                child: Text(_login ? 'Login' : 'SignUp'),
-              ),
-              OutlinedButton(
-                  onPressed: () async =>
-                      await ref.read(userPostsProvider.notifier).createPost(),
-                  child: const Text('Test')),
-              OutlinedButton(
-                  onPressed: () async => await _auth.logOut(),
-                  child: const Text('LogOut'))
-            ],
+        child: Center(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                if (!_login) ...[
+                  TextFormField(
+                    controller: _textEditName,
+                    decoration: _decoration('Name'),
+                  ),
+                  const SizedBox(height: 8),
+                ],
+                TextFormField(
+                  controller: _textEditEmail,
+                  decoration: _decoration('Email'),
+                ),
+                const SizedBox(height: 8),
+                TextFormField(
+                  controller: _textEditPass,
+                  decoration: _decoration('Password'),
+                  obscureText: true,
+                ),
+                OutlinedButton(
+                  onPressed: () async {
+                    _login
+                        ? await _auth.logIn('dash@email.com', 'dashword')
+                        : await _auth.signUp(
+                            'Dash', 'dash@email.com', 'dashword');
+          
+                    if (_auth.token != null && context.mounted) {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const PostsList()));
+                    }
+                  },
+                  child: Text(_login ? 'Login' : 'SignUp'),
+                ),
+                OutlinedButton(
+                    onPressed: () async =>
+                        await ref.read(userPostsProvider.notifier).createPost(),
+                    child: const Text('Test')),
+                OutlinedButton(
+                    onPressed: () async => await _auth.logOut(),
+                    child: const Text('LogOut'))
+              ],
+            ),
           ),
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          debugPrint('index $index');
+          if (index == 0) {
+            setState(() => _login = true);
+          } else {
+            setState(() => _login = false);
+          }
+        },
+        showSelectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.login_outlined), label: 'Login'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.create_outlined), label: 'SignUp'),
+        ],
       ),
     );
   }
