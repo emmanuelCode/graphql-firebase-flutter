b6637618e2e25dfd618ef5f5db0b6052e99f1d6b
emmanuelCode <e96e6@hotmail.com>
2023-12-01 02:24:11 -0500

    chore: polish and fix the ShowPostScreen

--- lib/views/show_post_screen.dart
+++ lib/views/show_post_screen.dart
@@ -18,20 +18,19 @@ class ShowPostScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          const SizedBox(height: 8),
-          Row(
-            children: [
-              Text(title),
-              const SizedBox(width: 8),
-              Text(created.toIso8601String())
-            ],
-          ),
-          Image.network(imageUrl),
-          const SizedBox(height: 8),
-          Text(text),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 8),
+            Text(title, style: Theme.of(context).textTheme.titleLarge),
+            Text(created.toIso8601String(),
+                style: Theme.of(context).textTheme.bodySmall),
+            const SizedBox(height: 8),
+            Image.network(imageUrl),
+            const SizedBox(height: 16),
+            Text(text, style: Theme.of(context).textTheme.bodyLarge),
+          ],
+        ),
       ),
     );
   }
